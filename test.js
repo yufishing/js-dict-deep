997550f06c7d6a53eba4800c68befe923278d96e
acstll <19@8302.net>
2013-09-25 08:34:39 +0200

    rewrite to add set and delete functionality

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var test = require('tape');
 var deep = require('./');
 
-test('deep access', function (t) {
+test('deep gets', function (t) {
   var obj = {
     foo: 'bar',
     bar: {
@@ -13,6 +13,41 @@ test('deep access', function (t) {
 
   t.equal(deep(obj, 'foo'), 'bar');
   t.equal(deep(obj, 'bar.baz.beep'), 'boop');
+  t.equal(deep(obj, 'bar.baz.beep.yep.nope'), undefined);
   t.end();
 });
 
+test('deep sets', function (t) {
+  var obj = {
+    foo: 'bar',
+    bar: {
+      baz: {
+        beep: 'boop'
+      }
+    }
+  };
+
+  t.equal(deep(obj, 'foo', 'yep'), 'yep');
+  t.equal(obj.foo, 'yep');
+  t.equal(deep(obj, 'bar.baz.beep', 'nope'), 'nope');
+  t.equal(obj.bar.baz.beep, 'nope');
+  t.end();
+});
+
+test('deep deletes', function (t) {
+  var obj = {
+    foo: 'bar',
+    bar: {
+      baz: {
+        beep: 'boop'
+      }
+    }
+  };
+
+  t.equal(deep(obj, 'foo', undefined), undefined);
+  t.notOk(obj.foo);
+  t.equal(deep(obj, 'bar.baz', undefined), undefined);
+  t.notOk(obj.bar.baz);
+  t.equal(deep(obj, 'bar.baz.beep'), undefined);
+  t.end();
+});
